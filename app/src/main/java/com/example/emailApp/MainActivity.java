be96fc02ea25bb9a07c6ec25acc536a3ebf84a7e
Chernenko-Valery <v02055020v@gmail.com>
2019-08-29 11:28:05 +0300

    replace Broadcastreceiver on Messenger

--- app/src/main/java/com/example/emailApp/MainActivity.java
+++ app/src/main/java/com/example/emailApp/MainActivity.java
@@ -1,30 +1,35 @@
 package com.example.emailApp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** Поле - приёмщик сообщений */
-    MailReceiver mMailReceiver = new MailReceiver();
+    ///** Поле - приёмщик сообщений */
+    //MailReceiver mMailReceiver = new MailReceiver();
 
     @Override
     protected void onCreate(Bundle aSavedInstanceState) {
         super.onCreate(aSavedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        registerReceiver(mMailReceiver, new IntentFilter(MailReceiver.UNREAD_MESSAGE_COUNT_INTENT_FLAG));
+        //registerReceiver(mMailReceiver, new IntentFilter(MailReceiver.UNREAD_MESSAGE_COUNT_INTENT_FLAG));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        unregisterReceiver(mMailReceiver);
+        //unregisterReceiver(mMailReceiver);
     }
 }
--- app/src/main/java/com/example/emailApp/SignInFragment.java
+++ app/src/main/java/com/example/emailApp/SignInFragment.java
@@ -3,14 +3,20 @@ package com.example.emailApp;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Messenger;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Фрагмент содержащий поля для ввода логина и пароля, а также кнопку, по которой осуществляется вход
@@ -27,6 +33,29 @@ public class SignInFragment extends Fragment {
     /** Поле - пароль */
     EditText mPassword;
 
+    /**
+     * Поле - Handler для обработки сообщений от MailIntentService
+     */
+    Handler mUiHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message aMsg) {
+            if(aMsg.what == MailIntentService.UNREAD_MESSAGE_COUNT_WHAT_TAG) {
+                if(aMsg.arg1 >= 0) {
+                    Toast toast = Toast.makeText(getContext(), "Количество непрочитанных сообщений = " + aMsg.arg1, Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(getContext(), "ошибка с кодом: " + aMsg.arg1, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        }
+    };
+
+    /**
+     * Поле - Messenger для взаимодействия с MailIntentService
+     */
+    Messenger mMessenger = new Messenger(mUiHandler);
+
     @Override
     public View onCreateView(final LayoutInflater aInflater, ViewGroup aContainer,
                              Bundle aSavedInstanceState) {
@@ -42,6 +71,7 @@ public class SignInFragment extends Fragment {
                 intent.putExtra(MailIntentService.LOGIN_TAG, mLogin.getText().toString() + "@yandex.com");
                 intent.putExtra(MailIntentService.PASSWORD_TAG, mPassword.getText().toString());
                 intent.putExtra(MailIntentService.SERVER_TAG, "yandex.com");
+                intent.putExtra(MailIntentService.MESSENGER_TAG, mMessenger);
                 getActivity().startService(intent);
             }
         });
