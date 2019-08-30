c5ab9b0c2acbdb671a73ad743f14b5d644eb53a7
Chernenko-Valery <v02055020v@gmail.com>
2019-08-30 10:31:45 +0300

    remove unused imports

--- app/src/main/java/com/example/emailApp/MailReceiver.java
+++ app/src/main/java/com/example/emailApp/MailReceiver.java
@@ -3,10 +3,7 @@ package com.example.emailApp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 
 /**
  * BroadcastReceiver for receiving message from MainIntentService
